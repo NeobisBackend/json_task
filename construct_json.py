d492e78c95c54bbcff50ba76bb9dee9b26634c92
Rysbai <rysbai.muslim@gmail.com>
2019-01-10 10:39:15 +0600

    Added json task and link to my repo to contributors list.

---
+++ construct_json.py
@@ -0,0 +1,24 @@
+import json
+from peronal_info import *
+
+
+upload_to = 'json_files/personal_info.json'
+data = {
+    "personal_info": {
+        "first_name": FIRST_NAME,
+        "last_name": LAST_NAME,
+        "age": AGE,
+        "address": ADDRESS,
+        "phone": PHONE,
+        "email": EMAIL,
+        "profession": PROFESSION,
+        "work_at": WORK_AT
+    }
+}
+
+def upload_data(upload_to, data):
+    with open(upload_to, 'w') as f_obj:
+        json.dump(data, f_obj)
+
+upload_data(upload_to, data)
+
---
+++ peronal_info.py
@@ -0,0 +1,9 @@
+
+FIRST_NAME = 'rysbai'
+LAST_NAME = 'amanbai uulu'
+AGE = 19
+ADDRESS = 'st. S.  Shedrin 44B'
+PHONE = '+996779583738'
+EMAIL = 'rysbai.coder@gmail.com'
+PROFESSION = 'programmer'
+WORK_AT = "neobis"
