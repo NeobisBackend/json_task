8425c3cf2d70b0e9f1a1bc79ce6ca094fcb081b2
Rysbai <rysbai.muslim@gmail.com>
2019-01-11 20:14:16 +0600

    Deleted files

--- peronal_info.py
+++
@@ -1,9 +0,0 @@
-
-FIRST_NAME = 'rysbai'
-LAST_NAME = 'amanbai uulu'
-AGE = 19
-ADDRESS = 'st. S.  Shedrin 44B'
-PHONE = '+996779583738'
-EMAIL = 'rysbai.coder@gmail.com'
-PROFESSION = 'programmer'
-WORK_AT = "neobis"
